01255fbc0aefa6f36dbddc23c07a313f17d8811e
xiahangli <2427417167@qq.com>
2019-10-19 11:10:43 +0800

    leetcode70题

--- src/main/java/server/leetcode/ValidParenthethese.java
+++ src/main/java/server/leetcode/ValidParenthethese.java
@@ -7,19 +7,27 @@ import java.util.Stack;
  */
 public class ValidParenthethese {
     public static void main(String[] args) {
-
+        boolean valid = isValid("(");
+        System.out.println(valid);
     }
 
     public static boolean isValid(String s) {
         char[] chars = s.toCharArray();//
         Stack<Character> stack = new Stack<Character>();
-        for (int i = chars.length-1; i >=0 ; i++) {//从第一个开始遍历
+        for (int i = 0; i <= chars.length - 1; i++) {//从第一个开始遍历
             char aChar = chars[i];
-            if (aChar == '('|| aChar  == '[' || aChar ==  '{'){
+            if (aChar == '(' || aChar == '[' || aChar == '{') {
                 stack.push(aChar);
-            }else if (stack.empty()){
+            } else if (stack.empty()) {//右括号 case1若当前栈为null,push右括号肯定不行
                 return false;
-            }else if (stack.peek()==)//栈非空
+            } else if ( (stack.peek() == '('&&chars[i]==')')
+                    ||  (stack.peek() == '[' &&chars[i]==']')
+                    || (stack.peek() == '{' && chars[i]=='}')) {//case2 栈内为左括号，弹出左括号
+                stack.pop();
+            }else{//case3 栈内为右括号
+                return false;
+            }
         }
+        return stack.empty();
     }
 }
