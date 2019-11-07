a39850ae4e562727fce747b3d10f7ee3a5ebea4f
xiahangli <2427417167@qq.com>
2019-11-08 00:35:37 +0800

    tupina

---
+++ src/main/java/server/ResponseResult.java
@@ -0,0 +1,36 @@
+package server;
+
+import java.util.Map;
+
+public class ResponseResult {
+    private Integer code;
+
+    private String message;
+
+    private Map<String, Object> result;
+
+    public Integer getCode() {
+        return this.code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Map<String, Object> getResult() {
+        return this.result;
+    }
+
+    public void setResult(Map<String, Object> result) {
+        this.result = result;
+    }
+
+}
--- src/main/java/server/UploadServlet.java
+++ src/main/java/server/UploadServlet.java
@@ -10,60 +10,89 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class UploadServlet extends HttpServlet {
 
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setHeader("Access-Control-Allow-Origin", "*");
         super.doPost(request, response);
 
-        FileItemFactory factory = new DiskFileItemFactory();
-
-        // 创建文件上传处理器
-        ServletFileUpload upload = new ServletFileUpload(factory);
-
-        // 开始解析请求信息
-        List items = null;
         try {
-            items = upload.parseRequest(request);
-        } catch (FileUploadException e) {
+            // 获取客户端传过来图片的二进制流
+            InputStream stream = request.getInputStream();
+            // 以当前时间戳为图片命名
+            SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
+            String strCurrentTime = df.format(new Date());
+            //最终文件存储位置
+            String imagePath = "C://Image/" + strCurrentTime + ".png";
+            // 这里的文件格式可以自行修改，如.jpg
+            FileOutputStream fos = new FileOutputStream(imagePath);
+            byte[] bbuf = new byte[32];
+            int hasRead = 0;
+            while ((hasRead = stream.read(bbuf)) > 0) {
+                fos.write(bbuf, 0, hasRead);
+                // 将文件写入服务器的硬盘上
+            }
+            fos.close();
+            stream.close();
+            /*
+             * 但是需要注意，采用这种原始的方式写入文件时，你会发现被写入的文件内容前4行并非是读取文件的真正内容，
+             * 从第四行开始才是正文数据。第二行是文件路径以及名称。所以通常的做法是，先将文件写入临时文件中，然后
+             * 再采用RandomAccessFile读取临时文件的第四行以后部分。写入到目标文件中。
+             */
+            Byte n;
+            // 读取临时文件
+            RandomAccessFile random = new RandomAccessFile(imagePath, "r");
+            int second = 1;
+            String secondLine = null;
+            while (second <= 2) {
+                secondLine = random.readLine();
+                second++;
+            }
+            int position = secondLine.lastIndexOf('\\');
+            // 获取上传文件的名称
+            String fileName = secondLine.substring(position + 1, secondLine.length() - 1);
+            random.seek(0);
+            long forthEndPosition = 0;
+            int forth = 1;
+            while ((n = random.readByte()) != -1 && (forth <= 4)) {
+                if (n == '\n') {
+                    forthEndPosition = random.getFilePointer();
+                    forth++;
+                }
+            }
+            RandomAccessFile random2 = new RandomAccessFile(imagePath, "rw");
+            random.seek(random.length());
+            long endPosition = random.getFilePointer();
+            long mark = endPosition;
+            int j = 1;
+            while ((mark >= 0) && (j <= 6)) {
+                mark--;
+                random.seek(mark);
+                n = random.readByte();
+                if (n == '\n') {
+                    endPosition = random.getFilePointer();
+                    j++;
+                }
+            }
+            random.seek(forthEndPosition);
+            long startPoint = random.getFilePointer();
+            while (startPoint < endPosition - 1) {
+                n = random.readByte();
+                random2.write(n);
+                startPoint = random.getFilePointer();
+            }
+            random.close();
+            random2.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-        // 对所有请求信息进行判断
-        Iterator iter = items.iterator();
-        while (iter.hasNext()) {
-            FileItem item = (FileItem) iter.next();
-            // 信息为普通的格式
-            if (item.isFormField()) {
-                String fieldName = item.getFieldName();
-                String value = item.getString();
-                request.setAttribute(fieldName, value);
-            }
-            // 信息为文件格式
-            else {
-                String fileName = item.getName();
-                System.out.println(fileName);
-                int index = fileName.lastIndexOf("\\");
-                fileName = fileName.substring(index + 1);
-                request.setAttribute("realFileName", fileName);
-
-                String basePath = request.getRealPath("/images");
-                File file = new File(basePath, fileName);
-                try {
-                    item.write(file);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-            request.setAttribute("msg", "文件上传成功!");
-            getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
-        }
     }
 
     @Override
